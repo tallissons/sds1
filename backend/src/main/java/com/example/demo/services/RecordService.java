5d9f2dda19cc545a5bf7fe303e97ec789f02b031
Tallisson Souza <tallissons@gmail.com>
2020-09-15 10:50:12 -0300

    POST records

---
+++ backend/src/main/java/com/example/demo/services/RecordService.java
@@ -0,0 +1,40 @@
+package com.example.demo.services;
+
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.dto.RecordDTO;
+import com.example.demo.dto.RecordInsertDTO;
+import com.example.demo.entities.Game;
+import com.example.demo.entities.Record;
+import com.example.demo.repositories.GameRepository;
+import com.example.demo.repositories.RecordRepository;
+
+@Service
+public class RecordService {
+
+	@Autowired
+	private RecordRepository repository;
+	
+	@Autowired
+	private GameRepository gameRepository;
+	
+	@Transactional
+	public RecordDTO insert(RecordInsertDTO dto) {
+		
+		Record entity = new Record();
+		
+		entity.setName(dto.getName());
+		entity.setAge(dto.getAge());
+		entity.setMoment(Instant.now());
+		
+		Game game = gameRepository.getOne(dto.getGameId());
+		entity.setGame(game);
+		
+		entity = repository.save(entity);
+		return new RecordDTO(entity);
+	}
+}
